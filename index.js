db3ec5166cfcf15a1fcab32ce0d9fc00309c8d71
pedrovergueiroo <pedrolv.fsilva@gmail.com>
2026-01-08 15:06:26 -0300

    Entrega desafio lógica DIO 3

---
+++ index.js
@@ -0,0 +1,43 @@
+// Classe genérica para representar um herói
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    // Estrutura de decisão para definir o ataque conforme o tipo
+    switch (this.tipo.toLowerCase()) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+
+const heroi1 = new Heroi("Arthas", 30, "guerreiro");
+const heroi2 = new Heroi("Merlin", 150, "mago");
+const heroi3 = new Heroi("Shaolin", 40, "monge");
+const heroi4 = new Heroi("Hanzo", 25, "ninja");
+
+heroi1.atacar(); // O guerreiro atacou usando espada
+heroi2.atacar(); // O mago atacou usando magia
+heroi3.atacar(); // O monge atacou usando artes marciais
+heroi4.atacar(); // O ninja atacou usando shuriken
